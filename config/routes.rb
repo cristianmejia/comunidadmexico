6943c424c494f60fa7247113d669b8ea61756722
Cristian Mejia <cristian.mejia@gmail.com>
2017-07-21 22:29:51 -0700

    ?

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
   resources :nominees
   resources :polls
   resources :media
@@ -8,6 +8,18 @@ Rails.application.routes.draw do
   resources :posts
   resources :spaces
   resources :users
+  resources :spaces, :path => '/@' do
+    member do
+      put "like", to:    "spaces#upvote"
+      put "dislike", to: "spaces#downvote"
+    end
+    resources :polls, :defaults => { :format => 'html' } do
+      resources :candidates, :defaults => { :format => 'html' }
+    end
+    resources :posts, :defaults => { :format => 'html' }
+    resources :announcements, :defaults => { :format => 'html' }
+    resources :media, :defaults => { :format => 'html' }
+  end
   root to: 'visitors#index'
   get '/auth/:provider/callback' => 'sessions#create'
   get '/signin' => 'sessions#new', :as => :signin
