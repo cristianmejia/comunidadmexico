b62c82c70094bd036fcc41b356a84fe1b6c6dc18
Cristian Mejia <cristian.mejia@gmail.com>
2017-07-21 22:33:38 -0700

    devise added to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :invitable, :database_authenticatable, :registerable, :confirmable,
+         :recoverable, :rememberable, :trackable, :validatable, :mailchimp, :omniauthable
   enum role: [:user, :vip, :admin]
   after_initialize :set_default_role, :if => :new_record?
 
