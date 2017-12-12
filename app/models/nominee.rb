b2ec9fcafdd6a33d22f251eef1860431a9e58070
Cristian Mejia <cristian.mejia@gmail.com>
2017-12-11 22:43:03 -0800

    Update nominee.rb

--- app/models/nominee.rb
+++ app/models/nominee.rb
@@ -1,3 +1,7 @@
 class Nominee < ApplicationRecord
+	scope :published, -> {where(:published => true)}
+  mount_uploader :avatar, FileUploader
+  acts_as_votable
+  is_impressionable
   belongs_to :poll
 end
