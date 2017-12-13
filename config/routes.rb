c95a4f285cba77fe1b32ef0103dc76c713cb15b9
Cristian Mejia <cristian.mejia@gmail.com>
2017-12-12 21:42:49 -0800

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -21,14 +21,5 @@ Rails.application.routes.draw do
     resources :media, :defaults => { :format => 'html' }
   end
   root to: 'visitors#index'
-  devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
-    devise_scope :user do
-      authenticated :user do
-        root 'home#index', as: :authenticated_root
-    end
-
-    unauthenticated do
-      root 'devise/sessions#new', as: :unauthenticated_root
-    end
-  end
+  devise_for :users
 end
