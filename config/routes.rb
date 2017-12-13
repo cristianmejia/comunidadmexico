30070ae647b776e571fe59475b35313d8fce8f3a
Cristian Mejia <cristian.mejia@gmail.com>
2017-12-12 21:24:38 -0800

    update

--- config/routes.rb
+++ config/routes.rb
@@ -22,4 +22,13 @@ Rails.application.routes.draw do
   end
   root to: 'visitors#index'
   devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
+    devise_scope :user do
+      authenticated :user do
+        root 'home#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
 end
