4617bc46f33e92750e20d15a61218c06cf5c9a8d
Cristian Mejia <cristian.mejia@gmail.com>
2018-01-13 20:06:16 -0800

    fix

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -31,8 +31,8 @@ class SpacesController < ApplicationController
   # POST /spaces
   # POST /spaces.json
   def create
-    # @space = Space.new(space_params)
-    @space = current_user.spaces.new(space_params)
+    // @space = Space.new(space_params)
+    @space = current_user.spaces.new(params[:space])
 
     respond_to do |format|
       if @space.save
