9e2d7102e14c316f74fb7ac47ee6d165a873d2c7
Cristian Mejia <cristian.mejia@gmail.com>
2018-03-29 00:01:58 -0700

    update

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -4,6 +4,6 @@ class VisitorsController < ApplicationController
 		@topSpaces = Space.all.order(cached_weighted_score: :desc).limit(5)
 		@topPolls = Poll.all.order(cached_weighted_score: :desc).limit(5)
 		@topNominees = Nominee.all.order(cached_weighted_score: :desc).limit(5)
-		@allSpaces = Space.all
+		@allSpaces = Space.all.order(name: :asc).limit(5)
 	end
 end
