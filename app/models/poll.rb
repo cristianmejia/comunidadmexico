9f85f2d556056362f14f16df017221b37f93fac0
cristianmejia <cristian.mejia@gmail.com>
2019-03-25 21:38:07 -0700

    poll param

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -3,4 +3,8 @@ class Poll < ApplicationRecord
   scope :published, -> {where(:published => true)}
   acts_as_votable
   is_impressionable
+  def to_param
+    @poll_friendlyurl = '@' + [user_id, User.find(user_id).name.to_s.parameterize].join("-") + '/~' + [space_id, User.find(space_id).name.to_s.parameterize].join("-") + '/quiz/' + [id, title.parameterize].join("-")
+    @poll_url = [id, title.parameterize].join("-")
+  end
 end
