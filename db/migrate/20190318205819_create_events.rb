4ff3fb3e9021d71b6e302768a1955f6c494707dc
cristianmejia <cristian.mejia@gmail.com>
2019-03-25 20:25:42 -0700

    fixing db migration

--- db/migrate/20190318205819_create_events.rb
+++ db/migrate/20190318205819_create_events.rb
@@ -1,9 +1,9 @@
 class CreateEvent < ActiveRecord::Migration[5.1]
   def change
-    create_table :experiences do |t|
+    create_table :events do |t|
       t.string :provider
       t.string :consumer
-      t.references :space, foreign_key: true
+      t.references :experience, foreign_key: true
       t.timestamp :started
       t.timestamp :finished
       t.integer :status
