aa62271d7004bb3972c751ad131f82bd537a37cf
cristianmejia <cristian.mejia@gmail.com>
2019-03-25 21:58:58 -0700

    to_param fix

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -5,7 +5,7 @@ class Poll < ApplicationRecord
   is_impressionable
 
   def to_param
-    @poll_friendlyurl = '@' + [user_id, User.find(user_id).name.to_s.parameterize].join("-") + '/~' + [space_id, User.find(space_id).name.to_s.parameterize].join("-") + '/quiz/' + [id, title.parameterize].join("-")
+    @poll_friendlyurl = '@' + [space_user_id, User.find(space_user_id).name.to_s.parameterize].join("-") + '/~' + [space_id, Space.find(space_id).name.to_s.parameterize].join("-") + '/quiz/' + [id, title.parameterize].join("-")
     @poll_url = [id, title.parameterize].join("-")
   end
 
