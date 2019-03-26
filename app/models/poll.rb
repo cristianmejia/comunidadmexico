01a278c661a28da365a3175faae13f78794f11bf
cristianmejia <cristian.mejia@gmail.com>
2019-03-25 21:42:18 -0700

    polls path definiton

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -3,8 +3,13 @@ class Poll < ApplicationRecord
   scope :published, -> {where(:published => true)}
   acts_as_votable
   is_impressionable
+
   def to_param
     @poll_friendlyurl = '@' + [user_id, User.find(user_id).name.to_s.parameterize].join("-") + '/~' + [space_id, User.find(space_id).name.to_s.parameterize].join("-") + '/quiz/' + [id, title.parameterize].join("-")
     @poll_url = [id, title.parameterize].join("-")
+  end
+
+  def polls_path
+    [space_id, User.find(space_id).name.to_s.parameterize].join("-") + '/quiz'
   end
 end
